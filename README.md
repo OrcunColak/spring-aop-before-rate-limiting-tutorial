b065d53a097637e5d7b38eb088ee979743d81399
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-05 08:28:19 +0300

    execution aspect

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 The original idea is from  
 https://levelup.gitconnected.com/rate-limiting-in-spring-boot-52220ba272c6
+
+This project uses "annotation" and "execution" for before aspect
---
+++ src/main/java/com/colak/springtutorial/logging/LoggingAspect.java
@@ -0,0 +1,17 @@
+package com.colak.springtutorial.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Before("execution(* com.colak.springtutorial.controller.*.*(..))")
+    public void logBeforeMethod() {
+        log.info("Method is about to be executed...");
+    }
+}
